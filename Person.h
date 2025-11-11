c7025c8b4af6d040080840b465d9d19736f83cfa
zaghal007 <abdelrahmanelzaghal2015@gmail.com>
2025-11-11 09:57:53 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,28 @@
-#ifndef PERSON_H
-#define PERSON_H
-
-#include <string>
+#ifndef PERSON_H_INCLUDED
+#define PERSON_H_INCLUDED
+#include<string>
 using namespace std;
 
-//#write Person class here
+class person {
+private :
+    string name ;
+    int id ;
+public :
+    person (string name , int id);
+    person();
+    display() ;
 
-
-
-
-
-
-
-
-
+};
 
 
 
 #endif
+
+
+
+
+
+
+
+
+
