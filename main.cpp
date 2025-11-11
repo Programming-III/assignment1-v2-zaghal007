9fadcd5b56101299a7fcf41f1739e7c4d80c9b99
zaghal007 <abdelrahmanelzaghal2015@gmail.com>
2025-11-11 10:04:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
-#include <string>
-#include "Person.h"
-#include "Student.h"
-#include "Instructor.h"
-#include "Course.h"
-
-
+#include<string>
+#include"Person.h"
+#include"Student.h"
+#include"Instructor.h"
+#include"Course.h"
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+void Person :: person(string name , int id ) {
+
+this->name = name ;
+this->id = id ;
+
+}
+
+ void Person :: display () {
+
+cout<< name<<endl ;
+cout<<id<<endl;
+
+}
+
+
+void Student :: display(){
+
+cout<< "Student Info:"<<endl;
+cout<<"Name:"<<name<<endl;
+cout<<"year:"<<yearlevel<<endl ;
+cout<<"Major:"<<major<<endl ;
+
+}
+
+void Instructor :: display (){
+
+
+cout<<"Instructor Info:"<<endl ;
+cout<<"Name:"<<name<<endl;
+cout <<"Department:"<<department<<endl:
+cout <<"Experience:"<<experienceyears<<endl;
+
+
+}
+
+void Course :: isplayCourseInfo(){ 
+    cout<<"Course:"<<courseName<<endl ; 
+    cout<<"Max Students:"<<maxStudents<<endl; 
+
+
+
+
+}
 
 
 
@@ -17,32 +57,13 @@ using namespace std;
 
 
 
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
+int main() 
+{ 
     
+    Person myperson("ali", 18897); 
+    Student mystudent(2 , "Informatics"); 
+    Instructor myinstructor("Computer Science", 5); 
+    Course mycourse ("CS101 - Introduction to Programming", 3 , "Omar Nabil (ID: 2202)")
+    cout << "Hello worlffffd!" << endl;
     return 0;
 }
