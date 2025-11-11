a5ce47533dfeab0486f05a96cc009ac4c6621e7c
zaghal007 <abdelrahmanelzaghal2015@gmail.com>
2025-11-11 09:58:23 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,19 +1,22 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
+#include<string>
+using namespace std;
 
+class student : public person {
 
+private :
+    int yearlevel ;
+    string major ;
+public :
+    studen();
+    student(int yearlevel, string major, string name , int id  )
+    : person(name, id), studentyearlevel(yearlevel), studnetmajor (major){}
 
+    display();
 
-
-
-
-
-
+};
 
 
 
